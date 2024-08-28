704943d6dca511467e53aa74f7b66fd0f6092928
achrafkeddour <152210432+achrafkeddour@users.noreply.github.com>
2024-08-28 18:53:06 +0100

    Update and rename rectanglecalc.py to rectanglecircle.py

--- 1-10 exercises/rectanglecircle.py
+++ 1-10 exercises/rectanglecircle.py
@@ -22,3 +22,19 @@ if bool(height) and bool(width):
         print("please enter 1 or 2 or 3")
 else: 
     print("please enter both height and width values")
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+#circle
+import math
+
+D = float(input("what is the diameter of the circle : "))
+R = D/2
+area = math.pi * pow(R,2)
+perimeter = math.pi * R * 2
+
+print(f"the area is {round(area,2)}")
+print(f"the perimeter is {round(perimeter,2)}")
+---------------------------------------------------------------------------------------------------------------------------------
+#triangle
+c = math.sqrt(pow(A,2)+pow(B,2)) #watr ta3 triangle (khati circle w rectangle)
