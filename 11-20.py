3c65f5b3abf971467414fb1ab844fe8a7c8fa6af
achrafkeddour <152210432+achrafkeddour@users.noreply.github.com>
2024-08-29 04:22:38 +0100

    Update 11-20.py

--- 11-20.py
+++ 11-20.py
@@ -1,7 +1,15 @@
-#logical operators : and , or , not 
+#11 logical operators : and , or , not 
 is_student = False
 if not is_student:
     print("you are not a studnet ")
 else :
     print("you're student")
 #you are not a studnet
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#12, conditional expressions : 'Action1 if condition else Action2' 
+age = 19
+print("Adult" if age >= 18 else "child")
+access_level = "admin" #i can change it
+print("full_access" if access_level == "admin" else "limited access")
+a = 12 ; b = 5
+print(f"the maximun is {a if a > b else b}")
