50cfff60b5551c9ac84957ed213d491e6d038661
achrafkeddour <152210432+achrafkeddour@users.noreply.github.com>
2024-08-30 18:04:07 +0100

    Update 11-20.py

--- 11-20.py
+++ 11-20.py
@@ -16,3 +16,11 @@ print(f"the maximun is {a if a > b else b}")
 temperature = float(input("enter the temperature today : "))
 is_sunny = "it is sunny today" if temperature > 23 else "it is not sunny be carefull bro"
 print(is_sunny)
+# 13 : count and replace methods -----------------------------------------------------------------------------------------------------------------------------------------
+value = input("enter a value : ")
+num_of_a = value.count("a") + value.count("A") 
+print(f"a has appeared {num_of_a} times")
+
+newname = value.replace("a","")
+print(f"if we deleted it, we'll have : {newname}")
+--------------------------------------------------------------------------------------------------------------------------------------------------------
