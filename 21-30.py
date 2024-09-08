a2d4feae6059f280e7a687113f357995164bfbed
achrafkeddour <152210432+achrafkeddour@users.noreply.github.com>
2024-09-08 10:16:15 +0100

    Update 21-30.py

--- 21-30.py
+++ 21-30.py
@@ -43,4 +43,29 @@ myset.add('newhaha') # or remove('existing_element')
 print(myset) #'achraf', 'newhaha', 'qq', 'gogo'}
 # pop() remove randomly because no order 
 #clear()
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#23 2D-collections:
+coll = [
+    ['days/times','9:00','11:00','13:00','14:00'],
+    ['sunday','mathematics','arabic','french','english'],
+    ['monday','geology','biology'],
+    ['thuesday','sports','science','history']
+]
+print('\n\t\tOur Work Plan ! \n')
+for row in coll:
+    for column in row:
+        print(f"{column:<15}", end=' | ')
+    print(); print()
 
+print('\n\nphone numbers :\n\n')
+nums = ((1,2,3),
+        (4,5,6),
+        (7,8,9),
+        ('*',0,'#')
+        )
+for row in nums:
+    for column in row:
+        print(f"{column:^3}",end=' ')
+
+    print()
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------
